5720e2c8b44df0c0e16d76817c0899d0af1b42a4
Oetzie <info@oetzie.nl>
2017-09-15 12:36:42 +0200

    Permission fix widget

--- core/components/googleanalytics/elements/widgets/realtime.widget.php
+++ core/components/googleanalytics/elements/widgets/realtime.widget.php
@@ -40,39 +40,49 @@
 		 * @return String.
 		 */
 		public function render() {
-			$this->googleanalytics = $this->modx->getService('googleanalytics', 'GoogleAnalytics', $this->modx->getOption('googleanalytics.core_path', null, $this->modx->getOption('core_path').'components/googleanalytics/').'model/googleanalytics/');
-			
-			if (false !== ($account = $this->googleanalytics->getAuthorizedProfile())) {
-				$this->modx->regClientCSS($this->googleanalytics->config['css_url'].'mgr/googleanalytics.css');
+			if ($this->checkPermissions()) {
+				$this->googleanalytics = $this->modx->getService('googleanalytics', 'GoogleAnalytics', $this->modx->getOption('googleanalytics.core_path', null, $this->modx->getOption('core_path').'components/googleanalytics/').'model/googleanalytics/');
 				
-				$this->modx->regClientStartupScript($this->googleanalytics->config['js_url'].'mgr/googleanalytics.js');
-				
-				$this->modx->regClientStartupHTMLBlock('<script type="text/javascript">
-					Ext.onReady(function() {
-						GoogleAnalytics.config = '.$this->modx->toJSON(array_merge($this->googleanalytics->config, array(
-                            'authorized'			=> $this->googleanalytics->isAuthorized(),
-                            'authorized_profile'	=> $this->googleanalytics->getAuthorizedProfile()
-                        ))).';
-					});
-				</script>');
-				
-				$this->modx->regClientStartupScript($this->googleanalytics->config['js_url'].'mgr/sections/realtime.widget.js');
-
-				if (is_array($this->googleanalytics->config['lexicons'])) {
-					foreach ($this->googleanalytics->config['lexicons'] as $lexicon) {
-						$this->modx->controller->addLexiconTopic($lexicon);
+				if (false !== ($account = $this->googleanalytics->getAuthorizedProfile())) {
+					$this->modx->regClientCSS($this->googleanalytics->config['css_url'].'mgr/googleanalytics.css');
+					
+					$this->modx->regClientStartupScript($this->googleanalytics->config['js_url'].'mgr/googleanalytics.js');
+					
+					$this->modx->regClientStartupHTMLBlock('<script type="text/javascript">
+						Ext.onReady(function() {
+							GoogleAnalytics.config = '.$this->modx->toJSON(array_merge($this->googleanalytics->config, array(
+	                            'authorized'			=> $this->googleanalytics->isAuthorized(),
+	                            'authorized_profile'	=> $this->googleanalytics->getAuthorizedProfile()
+	                        ))).';
+						});
+					</script>');
+					
+					$this->modx->regClientStartupScript($this->googleanalytics->config['js_url'].'mgr/sections/realtime.widget.js');
+	
+					if (is_array($this->googleanalytics->config['lexicons'])) {
+						foreach ($this->googleanalytics->config['lexicons'] as $lexicon) {
+							$this->modx->controller->addLexiconTopic($lexicon);
+						}
+					} else {
+						$this->modx->controller->addLexiconTopic($this->googleanalytics->config['lexicons']);
 					}
-				} else {
-					$this->modx->controller->addLexiconTopic($this->googleanalytics->config['lexicons']);
+	
+					$this->widget->name = $this->modx->lexicon('googleanalytics.widget_visitors_title', array(
+						'property' => $account['property_id']
+					));
+					
+					return $this->modx->smarty->fetch($this->googleanalytics->config['templates_path'].'widget.realtime.tpl');
 				}
-
-				$this->widget->name = $this->modx->lexicon('googleanalytics.widget_visitors_title', array(
-					'property' => $account['property_id']
-				));
-				
-				return $this->modx->smarty->fetch($this->googleanalytics->config['templates_path'].'widget.realtime.tpl');
 			}
 	    }
+	    
+	    /**
+		 * @access public.
+		 * @returns Boolean.
+		 */	    
+		public function checkPermissions() {
+			return $this->modx->hasPermission('googleanalytics');
+		}
 	}
 	
 	return 'modDashboardWidgetGoogleAnalyticsRealtime';
